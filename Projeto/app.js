bb6c50627c0587038577c9e2668a5fee1de32efe
unknown <andrey.masiero@hotmail.com>
2020-10-05 19:06:40 -0300

    start

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const app = express()
-
-
-
 app.use(express.json())
-
 app.set('view-engie', 'ejs')
-
 app.use(express.urlencoded({extended: false}))
 
-const fs = require('fs');
-const fsextra = require('fs-extra')
-const yaml = require('js-yaml');
+// const fs = require('fs');
+// const fsextra = require('fs-extra')
+// const yaml = require('js-yaml');
 
-let env = process.env.NODE_ENV;
-console.log(env)
-if (env == undefined) {
-    env = 'dev';
-}
+// let env = process.env.NODE_ENV;
+// console.log(env)
+// if (env == undefined) {
+//     env = 'dev';
+// }
 
 // let data;
 
@@ -34,7 +29,7 @@ if (env == undefined) {
 const usuarios = require('./src/routes/usuarios.route');
 const interesses = require('./src/routes/interesse.route');
 const eventos = require('./src/routes/eventos.route');
-const blockedIps = ['192.168.15.161', '127.0.0.1']
+// const blockedIps = ['192.168.15.161', '127.0.0.1']
 
 // app.use(function (req, res, next) {
 //     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
